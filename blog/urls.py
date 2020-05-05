c0c5b72410d206761632f1b03203833c8a68bb4b
桐山雄介 <kiriyamayusuke@kiriyamayuusukenoMacBook-Pro.local>
2020-05-05 11:17:55 +0900

    q

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='unique to yusukekon'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
